c3acfd9726953e6252c745f7c68a0cdd64561166
Aaron <aarongladstone456@gmail.com>
2019-05-13 16:43:27 -0500

    ?

--- project-3/client/src/Pages/UserPage.js
+++ project-3/client/src/Pages/UserPage.js
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import Wrapper from "../Components/Wrapper/index"
 import {Welcome} from "../Components/Banner/index"
-import CarDrop from "../Components/Dropdowns/index"
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -9,14 +8,154 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import "./style.css";
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import API from '../utils/API'
+import  {/* List, ListItem,*/ TabContainer } from "../Components/List/List";
+import TPList, { TPItem } from "../Components/Dropdowns/index"
+import TripLog from "../Components/Trip Log/index"
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {Table, TableRow} from '../Components/Trip Table/TripTable'
+
+    const styles = theme => ({
+      root: {
+        flexGrow: 1,
+      },
+      paper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
+    });
 
 
 class UserPage extends Component {
-
   state = {
     open: false,
-    open1: false
+    open1: false,
+    nickname: "",
+    plate: "",
+    initialMileage: "",
+    oilMileage: "",
+    filterMileage: "",
+    tireMileage: "",
+    batMileage: "",
+    brakeMileage: "",
+    // startingMileage: "",
+    // startingAddress: "",
+    // endAddress: "",
+    totalmiles: "",
+    date: "",
+    mileageType: "",
+    CarName: [],
+    Trip: [],
+    TripType: [],
+    TripPurposeId: "",
+    value: 0,
+    CarId: "",
+    purpose: "",
+    loadingCar: false,
+    loadingTrip: false,
+    selectedCar: null
+
   };
+
+  componentDidMount() {
+    this.loadCars();
+    this.loadTripTypes();
+    // this.loadTrip(this.state.selectedCar);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.loadingCar) {
+
+      API.saveCar({
+        nickname: this.state.nickname,
+        plate: this.state.plate,
+        initialMileage: this.state.initialMileage,
+        oilMileage: this.state.oilMileage,
+        filterMileage: this.state.filterMileage,
+        tireMileage: this.state.tireMileage, 
+        batMileage: this.state.batMileage,
+        brakeMileage: this.state.brakeMileage
+      })
+      .then(res => {
+        // this.setState({ CarName: [...this.state.cars, res]})
+        this.loadCars()
+      })
+      .catch(err => {
+        console.log(err)
+      });
+    }
+    // console.log(this.state.loadingTrip)
+    if (this.state.loadingTrip) {
+      // console.log("TRIP WORKING?")
+      API.saveTrip({
+        CarId: this.state.selectedCar,
+        date: this.state.date,
+        totalmiles: this.state.totalmiles,
+        TripPurposeId: this.state.TripPurposeId,
+      }, 
+      console.log("HELLO" + this.state.CarId)
+      )
+      .then(res => {
+        // console.log(res, 'WORKING')
+        this.loadTrip(this.state.selectedCar) 
+      })
+      .catch(err => {
+        this.loadTrip(this.state.selectedCar)
+        console.log(err)
+      });
+    }
+
+  }
+
+
+  loadCars = () => {
+    return API.getCarName()
+      .then(res => this.setState({
+          ...this.state,
+          CarName: res.data,   
+          nickname: "",
+          plate: "",
+          initialMileage: "",
+          oilMileage: "",
+          filterMileage: "",
+          tireMileage: "", 
+          batMileage: "",
+          brakeMileage: "",
+          loadingCar: false,
+          selectedCar:res.data[0].id
+        }), this.loadTrip(this.state.selectedCar)
+      )
+      .catch(err => console.log(err));
+  }
+
+  loadTrip = () => {
+    API.getTrip()
+    .then(res =>       
+      this.setState({
+        Trip: res.data,
+        date: "",
+        totalmiles: "",
+        purpose: "",
+        CarId: this.state.selectedCar,
+        loadingTrip: false
+      })
+    ) 
+    .catch(err => console.log(err));
+  }
+
+  loadTripTypes = () => {
+    API.getTripType()
+    .then(res => this.setState({TripType: res.data, date: "", totalmiles: ""
+    }))
+    .catch(err => console.log(err));
+  }
 
   handleClickOpen = () => {
     this.setState({ open: true });
@@ -34,97 +173,257 @@ class UserPage extends Component {
     this.setState({ open1: false });
   };
 
-  // When this component mounts, grab the book with the _id of this.props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
-  
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  selectPurpose = key => {
+    // console.log(key);
+    this.setState({TripPurposeId: key})
+  }
+
+  selectCar = car => {
+    // console.log('firing')
+    this.setState({CarId: parseInt(car.id), selectedCar: car.id})
+  }
+
+  // resetTripForm = () => { 
+  //   this.setState({...this.state, date: "", totalmiles: "", purpose: ""
+  //   })
+  // }
+
+  // resetCarForm = () => { 
+  //   this.setState({...this.state,
+  //   nickname: "",
+  //   plate: "",
+  //   initialMileage: "",
+  //   oilMileage: "",
+  //   filterMileage: "",
+  //   tireMileage: "", 
+  //   batMileage: "",
+  //   brakeMileage: ""
+  //   })
+  // }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.setState({ open: false, loadingCar: true });
+  };
+
+  handleFormSubmit2 = event => {
+    event.preventDefault();
+    this.setState({ open1: false, loadingTrip: true });
+  };
 
 render() {    
-      
-  return (
-
-<div>
+    const { value } = this.state;
+    const { classes } = this.props;
+    return (
+      <div>
         <Wrapper>
         <Welcome />
-        <CarDrop> </CarDrop>
-        
-        {/* Add a Car dialog */}
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+
+    {this.state.CarName.length ? (      
+      <div className={classes.root} >
+       
+        <AppBar position="static" color="default">
+            <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+
+        {this.state.CarName.map(car => (
+            <div key={car.id} onClick={(e) => this.selectCar(car)}>
+              <Tab  label={car.nickname} key={car.value} id={car.id} value={car.id} ></Tab> 
+               {/* {this.loadTrip(this.state.selectedCar)} */}
+               
+            </div>
+            
+          ))}
+          </Tabs>
+        </AppBar>  
+        {/* <TabContainer></TabContainer> */}
+{console.log(this.state.selectedCar)} 
+{console.log(this.state.Trip)}
+        <Button className= "addCar" variant="outlined" color="primary" onClick={this.handleClickOpen}>
           Add a Car 
-        </Button>
+        </Button>  
+
+
+    </div>      
+         ) : (
+          <div>
+          <h3> Add a car to your Account: </h3>
+          <Button className="addCar" variant="outlined" color="primary" onClick={this.handleClickOpen}>
+              Add a Car 
+            </Button>
+          </div>
+          )    
+         }
+
         <Dialog
           open={this.state.open}
           onClose={this.handleClose}
           aria-labelledby="form-dialog-title"
         >
+
+
           <DialogTitle id="form-dialog-title">Car</DialogTitle>
-          <DialogContent>
+
+          <DialogContent >
             <DialogContentText>
              Add a Car to your profile
-            </DialogContentText>
-            <TextField
+            </DialogContentText> 
+            <Grid container spacing={24}>
+            <Grid item xs={6}>
+             <TextField
               autoFocus
+              onChange={this.handleInputChange}
               margin="dense"
-              id="cName"
+              id="nickname"
               label="Car Name"
+              name="nickname"
+              value={this.state.nickname}
               type="text"
               fullWidth
-            />          
-              <TextField
-              autoFocus
-              margin="dense"
-              id="make"
-              label="Make"
-              type="text"
-              fullWidth
+              required
             />
               <TextField
               autoFocus
-              margin="dense"
-              id="model"
-              label="Model"
-              type="text"
-              fullWidth
-            />
-              <TextField
-              autoFocus
-              margin="dense"
-              id="year"
-              label="Year"
-              type="text"
-              fullWidth
-            />
-              <TextField
-              autoFocus
+              onChange={this.handleInputChange}
               margin="dense"
               id="plate"
               label="Plate"
+              name="plate"
+              value={this.state.plate}
               type="text"
               fullWidth
+              required
             />
+
               <TextField
               autoFocus
+              onChange={this.handleInputChange}
               margin="dense"
-              id="mileage"
-              label="Mileage"
-              type="text"
+              id="initial-mileage"
+              label="Initial Mileage"
+              name="initialMileage"
+              value={this.state.initialMileage}
+              type="number"
+              fullWidth
+              required
+            />   
+
+
+              <TextField
+              autoFocus
+              onChange={this.handleInputChange}
+              margin="dense"
+              id="Mileage at last oil change"
+              label="Mileage at last oil change"
+              name="oilMileage"
+              value={this.state.oilMileage}
+              type="number"
+              fullWidth
+              required
+            /> 
+            </Grid>
+           <Grid item xs={6}>
+          <TextField
+              autoFocus
+              onChange={this.handleInputChange}
+              margin="dense"
+              id="Mileage at last air filter change"
+              label="Mileage at last air filter change"
+              name="filterMileage"
+              value={this.state.filterMileage}
+              type="number"
               fullWidth
             />
-          </DialogContent>
-          <DialogActions>
+            <TextField
+              autoFocus
+              onChange={this.handleInputChange}
+              margin="dense"
+              id="Mileage at last tire rotation"
+              label="Mileage at last tire rotation"
+              name="tireMileage"
+              value={this.state.tireMileage}
+              type="number"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              onChange={this.handleInputChange}
+              margin="dense"
+              id="Mileage at last battery change"
+              label="Mileage at last battery change"
+              name="batMileage"
+              value={this.state.batMileage}
+              type="number"
+              fullWidth
+            />
+             <TextField
+              autoFocus
+              onChange={this.handleInputChange}
+              margin="dense"
+              id="Mileage at last break check"
+              label="Mileage at last break check"
+              name="brakeMileage"
+              value={this.state.brakeMileage}
+              type="number"
+              fullWidth
+            /> 
+       </Grid>
+       </Grid>
+       <h6>* is a required field</h6>
+          </DialogContent> 
+         <DialogActions>   
             <Button onClick={this.handleClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={this.handleClose} color="primary">
+            <Button className="addCar" onClick={this.handleFormSubmit} color="primary">
               Add Car
             </Button>
           </DialogActions>
         </Dialog>
-
-
-        {/* Add a Trip dialog */}
+        <br></br><br></br><br></br>
+        <TripLog>
         <Button variant="outlined" color="primary" onClick={this.handleClickOpen1}>
           Add a Trip 
         </Button>
+        </TripLog>
+        <br></br>          <br></br>
+        <Table>
+        {this.state.Trip.length ? (
+          <Fragment>
+              <tr><td>Date</td><td>Mileage</td><td>Purpose</td></tr>
+
+               {this.state.Trip.map(triparr => (
+
+               <TableRow> <td>{triparr.date}</td><td>{triparr.totalmiles}</td><td>{triparr.Trip_Purpose.purpose}</td> </TableRow> 
+
+           
+            ))}      
+          </Fragment>
+           ) : (<h6> Add a Trip with your car </h6>)
+          }
+
+
+
+        </Table>
+        {/* Add a Trip dialog */}
+
         <Dialog
           open={this.state.open1}
           onClose={this.handleClose1}
@@ -138,74 +437,134 @@ render() {
             <br></br>
               <TextField
                 id="date"
+                onChange={this.handleInputChange}
                 label="Date of trip"
                 type="date"
-                defaultValue="2017-05-24"
+                name="date"
+                value={this.state.date}
+                defaultValue="date"
                 InputLabelProps={{
-                  shrink: true,
+                  shrink: true
                 }}
               />
-              <TextField
+              {/* <TextField
               autoFocus
               margin="dense"
               id="name"
               label="Starting Mileage"
+              name="startingMileage"
+              onChange={this.handleInputChange}
+              value={this.state.startingMileage}
               type="text"
               fullWidth
+              required
             />
              <TextField
               autoFocus
               margin="dense"
               id="name"
               label="Starting Address"
+              name="startingAddress"
+              onChange={this.handleInputChange}
+              value={this.state.startingAddress}
               type="text"
               fullWidth
+              required
             />
             <TextField
               autoFocus
               margin="dense"
               id="name"
               label="Ending Address"
+              name="endingAddress"
+              onChange={this.handleInputChange}
+              value={this.state.endingAddress}
               type="text"
               fullWidth
-            />
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Total Mileage"
-              type="text"
-              fullWidth
-            />
-          
-            <select id="user-list" required>
-            <option label="Select Trip Type"></option>
-              <option value="Personal" name="" >Personal</option>
-              <option value="Work" name="" >Work</option>
-            </select>
-            
+              required
+            /> */}
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Total Mileage"
+                name="totalmiles"
+                onChange={this.handleInputChange}
+                value={this.state.totalmiles}
+                type="number"
+                fullWidth
+                required
+              />
+              <p>Trip Type:</p>
+              {this.state.TripType.length ? (
+              <TPList onChange={(e) => {
+                this.selectPurpose(parseInt(e.target.value))
+              }}> 
+              <option label="Select Trip Type" disabled selected></option>
+              {this.state.TripType.map(trip => (
+                <TPItem key={trip.id} id={trip.id}>{trip.purpose}
+            </TPItem> 
+            ))}      
+          </TPList>
+          ) : (<h6> null </h6>)}
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose1} color="primary">
               Cancel
             </Button>
-            <Button onClick={this.handleClose1} color="primary">
+            <Button onClick={this.handleFormSubmit2}  color="primary">
               Add Trip
             </Button>
           </DialogActions>
         </Dialog>
 
         </Wrapper>
-        </div>
-
+      </div>
     );
-
+  }
 }
-}
 
-export default UserPage;
+      TabContainer.propTypes = {
+        children: PropTypes.node.isRequired,
+      };
+      UserPage.propTypes = {
+        classes: PropTypes.object.isRequired
+      }
+      UserPage.propTypes = {
+        classes: PropTypes.object.isRequired,
+      };
+      
+
+export default withStyles(styles)(UserPage);
+// export withStyles(styles)(UserPage);
 
 
+
+
+
+
+
+
+
+// const styles = theme => ({
+//   root: {
+//     flexGrow: 1,
+//     width: '100%',
+//     backgroundColor: theme.palette.background.paper,
+//   },
+// });
+
+
+
+
+
+
+
+
+
+
+  
+  
 
 
 
