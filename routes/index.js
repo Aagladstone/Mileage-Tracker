68a082677144ff4c64321072da27c60a6294e47e
liane880218 <lianesuarez88@gmail.com>
2019-05-20 21:15:20 -0500

    e

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,8 @@ const userRegister = require("./api/userRegister");
 const userLogin = require("./api/userLogin");
 const userLogout = require("./api/userLogout");
 const userGet= require("./api/userGet");
-<<<<<<< HEAD
-const maintRoutes = require("./api/maintenance")
-=======
 const maintRoutes = require("./api/maintenance");
 const carMaintRoutes = require("./api/clearMaint");
->>>>>>> 58838340a1723d1901b2123014e7c00ed32ee8ab
 
 // API Routes
 router.use("/api/car/:user?", carRoutes);
@@ -24,10 +20,7 @@ router.use("/logout", userLogout)
 router.use("/user", userGet)
 router.use("/api/trip/:car?", tripRoutes)
 router.use("/api/maintenance/:car?", maintRoutes)
-<<<<<<< HEAD
-=======
 router.use("/api/clearmaint", carMaintRoutes)
->>>>>>> 58838340a1723d1901b2123014e7c00ed32ee8ab
 
 
 
